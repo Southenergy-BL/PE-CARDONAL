263cba33d83adc81159c444aab7d235c4e1f9ffe
Southenergy-BL <bryan.levy@southenergy.cl>
2026-07-09 13:43:43 -0400

    Update Codigo_base_CARDONAL.py

--- Codigo_base_CARDONAL.py
+++ Codigo_base_CARDONAL.py
@@ -93,7 +93,7 @@ def calcular_rampas(df_agrupado):
 # ==========================================
 # 3. INTERFAZ Y PESTAÑAS
 # ==========================================
-st.title("Monitoreo de Portafolio de Activos Renovables")
+st.title("Monitoreo de Portafolio de Activos Renovables: STATKRAFT")
 
 RUTA_GEN = os.path.join("datos", "Generación 2025-Jun 2026.xlsx")
 RUTA_CMG = os.path.join("datos", "CMg_Multibarra_Con_Promedios.xlsx")
